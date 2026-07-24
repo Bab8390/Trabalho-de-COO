e4d3b6e2d4538348eef7ecd3b1ca00fc02014f2e
GigaDLBO <danielbraga1966@gmail.com>
2026-07-24 16:17:46 -0300

    Update and rename Background.Java to Background.java

---
+++ Background.java
@@ -0,0 +1,33 @@
+public abstract class Background{
+    protected double[] x;
+    protected double[] y;
+    protected double velocidade;
+    protected double count;
+
+    public Background(int estrelas, double velocidade) {
+        this.x = new double [estrelas];
+        this.y = new double [estrelas];
+        this.velocidade = velocidade;
+        this.count = 0.0;
+
+        for(int i = 0; i < estrelas; i++) {
+            this.x[i] = Math.random() * GameLib.WIDTH;
+            this.y[i] = Math.random() * GameLib.HEIGHT;
+        }
+    }
+    
+    public double getX() { return x; }
+    
+    public void setX(double x) { this.x = x; }
+
+    public double getY() { return y; }
+    
+    public void setY(double y) { this.y = y; }
+    
+    public double getVelocidade() { return velocidade; }
+    
+    public void setVelocidade(double velocidade) { this.velocidade = velocidade; }
+
+    
+    public abstract void desenhaBackground(long delta);
+}
