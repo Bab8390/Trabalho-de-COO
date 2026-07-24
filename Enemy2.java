75885cd89968f7dddb624bc2a1f52fed424e1210
GigaDLBO <danielbraga1966@gmail.com>
2026-07-24 18:48:45 -0300

    Update Enemy2.java

--- Enemy2.java
+++ Enemy2.java
@@ -1,96 +1,95 @@
 import java.util.ArrayList;
+import java.awt.Color; // 1. Import adicionado
 
 public class Enemy2 extends Enemy {
 
     public Enemy2(double x, double y) {
         this.x = x;
         this.y = y;
-        this.state = ACTIVE;
+        this.state = Main.ACTIVE; // Usando Main.
         this.radius = 12.0;
         this.velocidade = 0.42;
         this.angle = (3 * Math.PI) / 2;
         this.rv = 0.0;
     }
 
+    @Override
     public void desenhaEnemy(long currentTime) {
 
-            if (this.state == EXPLODING) {
+        if (this.state == Main.EXPLODING) {
+            double alpha = (currentTime - this.explosion_start) / (this.explosion_end - this.explosion_start);
+            GameLib.drawExplosion(this.x, this.y, alpha);
+        }
 
-                double alpha = (currentTime - this.explosion_start) / (this.explosion_end - this.explosion_start);
-                GameLib.drawExplosion(this.x, this.y, alpha);
-            }
-
-            if (this == ACTIVE) {
-
-                GameLib.setColor(Color.MAGENTA);
-                GameLib.drawDiamond(this.x, this.y, this.tamanho);
-            }
+        if (this.state == Main.ACTIVE) { // 2. Corrigido para this.state
+            GameLib.setColor(Color.MAGENTA);
+            GameLib.drawDiamond(this.x, this.y, this.radius); // 3. Corrigido para this.radius
         }
     }
 
-    public void atualizar(long delta, long currentTime, Player player, ArrayList<Projectile> TirosInimigo) {
+    @Override
+    public void atualizar(long delta, long currentTime, Player player, ArrayList<Projetil> TirosInimigo) { // 5. Ajustado para Projetil
 
-            if (this.state == EXPLODING) {
-
-                if (currentTime > this.explosion_end) {
-
-                    this.state = INACTIVE;
-                }
+        if (this.state == Main.EXPLODING) {
+            if (currentTime > this.explosion_end) {
+                this.state = Main.INACTIVE;
             }
+        }
 
-            if (this.state == ACTIVE) {
+        if (this.state == Main.ACTIVE) {
 
-                /* verificando se inimigo saiu da tela */
-                if (this.x < -10 || this.x > GameLib.WIDTH + 10) {
+            /* verificando se inimigo saiu da tela */
+            if (this.x < -10 || this.x > GameLib.WIDTH + 10) {
+                this.state = Main.INACTIVE; // 4. Removido o vetor antigo!
+            } else {
 
-                    enemy2_states[i] = INACTIVE;
-                } else {
+                boolean shootNow = false;
+                double previousY = this.y;
 
-                    boolean shootNow = false;
-                    double previousY = this.y;
+                this.x += this.velocidade * Math.cos(this.angle) * delta;
+                this.y += this.velocidade * Math.sin(this.angle) * delta * (-1.0);
+                this.angle += this.rv * delta;
 
-                    this.x += this.velocidade * Math.cos(this.angle) * delta;
-                    this.y += this.velocidade * Math.sin(this.angle) * delta * (-1.0);
-                    this.angle += this.rv * delta;
+                double threshold = GameLib.HEIGHT * 0.30;
 
-                    double threshold = GameLib.HEIGHT * 0.30;
+                if (previousY < threshold && this.y >= threshold) {
+                    if (this.x < GameLib.WIDTH / 2)
+                        this.rv = 0.003;
+                    else
+                        this.rv = -0.003;
+                }
 
-                    if (previousY < threshold && this.y >= threshold) {
+                if (this.rv > 0 && Math.abs(this.angle - 3 * Math.PI) < 0.05) {
+                    this.rv = 0.0;
+                    this.angle = 3 * Math.PI;
+                    shootNow = true;
+                }
 
-                        if (this.x < GameLib.WIDTH / 2)
-                            this.rv = 0.003;
-                        else
-                            this.rv = -0.003;
-                    }
+                if (this.rv < 0 && Math.abs(this.angle) < 0.05) {
+                    this.rv = 0.0;
+                    this.angle = 0.0;
+                    shootNow = true;
+                }
 
-                    if (this.rv > 0 && Math.abs(this.angle - 3 * Math.PI) < 0.05) {
+                if (shootNow) {
+                    
+                    // 7. Restaurado o pequeno for() para fazer o Tiro Múltiplo Espalhado
+                    double[] angles = { Math.PI / 2 + Math.PI / 8, Math.PI / 2, Math.PI / 2 - Math.PI / 8 };
 
-                        this.rv = 0.0;
-                        this.angle = 3 * Math.PI;
-                        shootNow = true;
-                    }
-
-                    if (this.rv < 0 && Math.abs(this.angle) < 0.05) {
-
-                        this.rv = 0.0;
-                        this.angle = 0.0;
-                        shootNow = true;
-                    }
-
-                    if (shootNow) {
-
-                        double[] angles = { Math.PI / 2 + Math.PI / 8, Math.PI / 2, Math.PI / 2 - Math.PI / 8 };
-
-                        double a = angles + Math.random() * Math.PI / 6 - Math.PI / 12;
+                    for (int k = 0; k < angles.length; k++) {
+                        
+                        double a = angles[k] + Math.random() * Math.PI / 6 - Math.PI / 12;
                         double vx = Math.cos(a);
                         double vy = Math.sin(a);
 
-                        Projetil_e projetil = new Projetil_e(this.x, this.y, vx * 0, 30, vy * 0, 30);
-                        projeteisEnemy.add(projetil);
+                        // 6. Corrigidas as vírgulas para pontos (0.30)
+                        Projetil_e projetil = new Projetil_e(this.x, this.y, vx * 0.30, vy * 0.30);
+                        
+                        // 5. Corrigido para TirosInimigo
+                        TirosInimigo.add(projetil);
                     }
                 }
             }
         }
     }
-
 }
