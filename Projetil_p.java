fad46071df95fbbe1ef0ddcde6cc5c86018a1a44
GigaDLBO <danielbraga1966@gmail.com>
2026-07-24 18:46:51 -0300

    Update Projetil_p.java

--- Projetil_p.java
+++ Projetil_p.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 public class Projetil_p extends Projetil {
 
     public Projetil_p(double x, double y) {
@@ -5,15 +7,15 @@ public class Projetil_p extends Projetil {
         this.y = y;
         this.vx = 0.0;
         this.vy = -1.0;
-        this.state = ACTIVE;
+        this.state = Main.ACTIVE;
+        this.radius = 1.0; 
     }
 
     @Override
     public void atualizar(long delta) {
-        if(this.state == ACTIVE) {
-            
-            if(this.y < 0) {
-                this.state = INACTIVE;
+        if (this.state == Main.ACTIVE) {
+            if (this.y < 0.0) {
+                this.state = Main.INACTIVE;
             } else {
                 this.x += this.vx * delta;
                 this.y += this.vy * delta;
@@ -23,12 +25,11 @@ public class Projetil_p extends Projetil {
 
     @Override
     public void desenhar() {
-        if(this.state == ACTIVE) {
+        if (this.state == Main.ACTIVE) {
             GameLib.setColor(Color.GREEN);
-            
-            GameLib.drawLine(this.x, this.y - 5, this.x, this.y + 5);
-            GameLib.drawLine(this.x - 1, this.y - 3, this.x - 1, this.y + 3);
-            GameLib.drawLine(this.x + 1, this.y - 3, this.x + 1, this.y + 3);
+            GameLib.drawLine(this.x, this.y - 5.0, this.x, this.y + 5.0);
+            GameLib.drawLine(this.x - 1.0, this.y - 3.0, this.x - 1.0, this.y + 3.0);
+            GameLib.drawLine(this.x + 1.0, this.y - 3.0, this.x + 1.0, this.y + 3.0);
         }
     }
 }
