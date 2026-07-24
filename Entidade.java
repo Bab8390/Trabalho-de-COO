84bd2704369ce51e72107c611a8c1085dbb298be
GigaDLBO <danielbraga1966@gmail.com>
2026-07-24 19:56:34 -0300

    Add method signatures for entity properties

--- Entidade.java
+++ Entidade.java
@@ -5,4 +5,12 @@ public interface Entidade {
 	public static final int EXPLODING = 2;
 
     //assinatura dos metodos
+
+    public double getX();
+    public void setX(double x);
+    public double getY();
+    public void setY(double y);
+    public int getState();
+    public void setState(int state);
+    public double getRadius();
 }
