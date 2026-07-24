be912ac7927b1e429ac02388d78410bbc96b0636
GigaDLBO <danielbraga1966@gmail.com>
2026-07-24 18:46:33 -0300

    Update Projetil_e.java

--- Projetil_e.java
+++ Projetil_e.java
@@ -1,33 +1,33 @@
 import java.awt.Color;
 
-public class Projetil_e extends Projetil {
+public class Projetil_p extends Projetil {
+   public Projetil_p(double var1, double var3) {
+      this.x = var1;
+      this.y = var3;
+      this.vx = (double)0.0F;
+      this.vy = (double)-1.0F;
+      this.state = 1;
+   }
 
-    public Projetil_e(double x, double y, double vx, double vy) {
-        this.x = x;
-        this.y = y;
-        this.vx = vx;
-        this.vy = vy;
-        this.state = Main.ACTIVE;
-        this.radius = 2.0; // Ele usa a variável da mãe automaticamente!
-    }
+   public void atualizar(long var1) {
+      if (this.state == 1) {
+         if (this.y < (double)0.0F) {
+            this.state = 0;
+         } else {
+            this.x += this.vx * (double)var1;
+            this.y += this.vy * (double)var1;
+         }
+      }
 
-    @Override
-    public void atualizar(long delta) {
-        if(this.state == Main.ACTIVE) {
-            if(this.y > GameLib.HEIGHT) {
-                this.state = Main.INACTIVE;
-            } else {
-                this.x += this.vx * delta;
-                this.y += this.vy * delta;
-            }
-        }
-    }
+   }
 
-    @Override
-    public void desenhar() {
-        if(this.state == Main.ACTIVE) { 
-            GameLib.setColor(Color.RED);
-            GameLib.drawCircle(this.x, this.y, this.radius);
-        }
-    }
+   public void desenhar() {
+      if (this.state == 1) {
+         GameLib.setColor(Color.GREEN);
+         GameLib.drawLine(this.x, this.y - (double)5.0F, this.x, this.y + (double)5.0F);
+         GameLib.drawLine(this.x - (double)1.0F, this.y - (double)3.0F, this.x - (double)1.0F, this.y + (double)3.0F);
+         GameLib.drawLine(this.x + (double)1.0F, this.y - (double)3.0F, this.x + (double)1.0F, this.y + (double)3.0F);
+      }
+
+   }
 }
