4e79f6001c34de8b7f44b1e8aff0d681f2a90a3b
GigaDLBO <danielbraga1966@gmail.com>
2026-07-26 11:08:36 -0300

    Update Enemy1.java

--- src/Enemy1.java
+++ src/Enemy1.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.awt.Color;
 
-public class Enemy1 extends Enemy { // 1. Corrigido: Adicionado o nome da classe
+public class Enemy1 extends Enemy { 
     private long nextShot;
     
     public Enemy1(double x, double y) {
         this.x = x;
         this.y = y;
         this.velocidade = 0.20 + Math.random() * 0.15;
-        this.state = Entidade.ACTIVE; // Refere-se à constante lá do Main
+        this.state = Entidade.ACTIVE; 
         this.radius = 9.0;
         this.angle = (3 * Math.PI) / 2;
         this.rv = 0.0;
@@ -17,10 +17,9 @@ public class Enemy1 extends Enemy { // 1. Corrigido: Adicionado o nome da classe
     public long getNextShot() { return nextShot; }
     public void setNextShot(long nextShot) { this.nextShot = nextShot; }
 
-    @Override // 2. O @Override protege contra erros de digitação no nome!
+    @Override !
     public void atualizar(long delta, long currentTime, Player player, ArrayList<Projetil> TirosInimigo){
         
-        // 3. Removido o FOR fantasma e corrigido 'states' para 'state'
         if(this.state == Entidade.EXPLODING){
             if(currentTime > this.explosion_end){
                 this.state = Entidade.INACTIVE;
@@ -29,11 +28,10 @@ public class Enemy1 extends Enemy { // 1. Corrigido: Adicionado o nome da classe
 
         if(this.state == Entidade.ACTIVE){
 
-            /* verificando se inimigo saiu da tela */
             if(this.y > GameLib.HEIGHT + 10) {
                 this.state = Entidade.INACTIVE;
             } else {
-                // 4. Trocado 'vy' pela variável correta 'velocidade'
+            '
                 this.x += this.velocidade * Math.cos(this.angle) * delta;
                 this.y += this.velocidade * Math.sin(this.angle) * delta * (-1.0);
                 this.angle += this.rv * delta;
@@ -41,7 +39,6 @@ public class Enemy1 extends Enemy { // 1. Corrigido: Adicionado o nome da classe
                 if(currentTime > this.nextShot && this.y < player.getY()){
                     Projetil_e projetil = new Projetil_e(this.x, this.y, Math.cos(this.angle) * 0.45, Math.sin(this.angle) * 0.45 * (-1.0));
                     
-                    // 5. Corrigido para o nome do parâmetro recebido
                     TirosInimigo.add(projetil); 
                     
                     this.nextShot = (long) (currentTime + 200 + Math.random() * 500);
@@ -52,7 +49,7 @@ public class Enemy1 extends Enemy { // 1. Corrigido: Adicionado o nome da classe
 
     @Override
     public void desenhaEnemy(long currentTime){ //vai desenhar o inimigo
-        // Corrigido de 'states' para 'state'
+       
         if(this.state == Entidade.EXPLODING){
             double alpha = (currentTime - this.explosion_start) / (this.explosion_end - this.explosion_start);
             GameLib.drawExplosion(this.x, this.y, alpha);
